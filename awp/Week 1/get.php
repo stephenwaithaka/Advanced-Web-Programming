af5d73a802ec5d6234a7f05c95208b3d5a11a60b
stephenwaithaka <stephen.waithaka@hotmail.com>
2016-04-22 13:36:02 +0300

    First

---
+++ awp/Week 1/get.php
@@ -0,0 +1,44 @@
+<div id="accordion">
+<?php
+    $host = 'mysql.metropolia.fi';
+    $dbname = 'stephenw'; // your username
+    $user = 'stephenw'; // your username
+    $pass = 'Emarkus2009'; // your database password
+    
+    try {
+            $DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+            $DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+            $DBH->query("SET NAMES 'UTF8';");
+        
+        } catch(PDOException $e) {
+            echo "Could not connect to database.";
+            file_put_contents('log.txt', $e->getMessage(), FILE_APPEND);
+        }
+
+try {
+    $eventList = array();
+    $sql = "SELECT * FROM calendar";
+    $STH = $DBH->query($sql);
+    $STH->setFetchMode(PDO::FETCH_OBJ);
+    while ($row = $STH->fetch()){
+    	echo '<h3>'.$row->eDate.'<br><strong>'.$row->eName.'</strong></h3><div><i>'.$row->eDescription.'</i><br><br>With:<br>'.$row->pEmail.'<br>'.$row->pPhone.'</div>';
+    }
+ } catch (PDOException $e) {
+	echo 'Something went wrong';
+	file_put_contents('log.txt', $e->getMessage()."\n\r", FILE_APPEND); // remember to set the permissions so that log.txt can be created
+}      
+
+?>
+</div>
+<script>
+// TODO: use jQuery UI to make an accordion from <div id="accordion">.
+
+
+// Set 'collapsible' property to 'true' and 'active' property to 'none'
+$( "#accordion").accordion({
+    active:"none",
+    collapsible:true,
+
+    });
+
+</script>
